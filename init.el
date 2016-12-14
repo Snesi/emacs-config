fe7aa9a22cc105f76b14249b9598eaa8e6e0d456
David G. Shannon <david.shannon@empaytech.com>
2016-12-14 15:36:45 +0100

    Add smooth scrolling.

--- init.el
+++ init.el
@@ -222,3 +222,8 @@
 
 (setq neo-smart-open t)
 (setq projectile-switch-project-action 'neotree-projectile-action)
+
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; scroll one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 1) ;; keyboard scroll one line at a time
